371aa5ec0fa3cc13086d25f2795cc716844dcd76
Paulo Baima <padasil@microsoft.com>
2019-11-20 12:25:36 -0300

    adding unify release unit tests

--- unify-release-build-task/tests/services/unifyReleaseServiceTests.ts
+++ unify-release-build-task/tests/services/unifyReleaseServiceTests.ts
@@ -28,7 +28,14 @@ describe('UnifyReleaseService', () => {
     })
 
     describe('unifyRelease', () => {
-        it("Should create tag if all last related builds are Completed and Succeeded or Partially Suceeded", async () => {
+        var relatedBuildsStub = null;
+        var buildServiceStub: StubbedInstance<IBuildService> = null
+        beforeEach(() => {
+            relatedBuildsStub = new Map<string, Build>();
+            buildServiceStub = StubInterface<IBuildService>();
+        })
+
+        function createBuildServiceStubs(relatedBuilds: Build[]) {
             let triggeredBuild: Build = {
                 id: 1,
                 definition: {
@@ -37,6 +44,22 @@ describe('UnifyReleaseService', () => {
                 status: BuildStatus.InProgress,
                 sourceVersion: "sourceVersion"
             };
+
+            var allBuilds = [triggeredBuild].concat(relatedBuilds);
+
+            buildServiceStub.getBuildInfo
+                .withArgs(
+                    configStub.teamFoundationCollectionUri, configStub.accessToken, configStub.teamFoundationProject, configStub.currentBuildId)
+                .returns(Promise.resolve(triggeredBuild));
+
+            buildServiceStub.listRelatedBuilds
+                .withArgs(
+                    configStub.teamFoundationCollectionUri, configStub.accessToken, configStub.teamFoundationProject, "sourceVersion", configStub.waitForAllTriggeredBuilds, [configStub.definition1, configStub.definition2, configStub.definition3, configStub.definition4, configStub.definition5])
+                .returns(Promise.resolve(relatedBuildsStub));
+
+        }
+        it("Should create tag if all last related builds are Completed and Succeeded or Partially Suceeded", async () => {
+
             let build2: Build = {
                 id: 2,
                 definition: {
@@ -55,23 +78,9 @@ describe('UnifyReleaseService', () => {
                 result: BuildResult.PartiallySucceeded,
                 sourceVersion: "sourceVersion"
             };
-            var relatedBuildsStub = new Map<string, Build>();
-            relatedBuildsStub.set(triggeredBuild.definition.id.toString(), triggeredBuild);
-            relatedBuildsStub.set(build2.definition.id.toString(), build2);
-            relatedBuildsStub.set(build3.definition.id.toString(), build3);
 
-            let buildServiceStub: StubbedInstance<IBuildService> = StubInterface<IBuildService>();
-
-            buildServiceStub.getBuildInfo
-                .withArgs(
-                    configStub.teamFoundationCollectionUri, configStub.accessToken, configStub.teamFoundationProject, configStub.currentBuildId)
-                .returns(Promise.resolve(triggeredBuild));
-
-            buildServiceStub.listRelatedBuilds
-                .withArgs(
-                    configStub.teamFoundationCollectionUri, configStub.accessToken, configStub.teamFoundationProject, "sourceVersion", configStub.waitForAllTriggeredBuilds, [configStub.definition1, configStub.definition2, configStub.definition3, configStub.definition4, configStub.definition5])
-                .returns(Promise.resolve(relatedBuildsStub));
-
+            createBuildServiceStubs([build2, build3]);
+            
             let buildServiceCreateTagSpy = buildServiceStub.addBuildTag
                 .withArgs(configStub.teamFoundationCollectionUri, configStub.teamFoundationProject, configStub.accessToken, configStub.currentBuildId, configStub.releaseTag);
 
@@ -79,16 +88,48 @@ describe('UnifyReleaseService', () => {
 
             await unifyReleaseService.unifyRelease();
 
-            debugger;
             expect(buildServiceCreateTagSpy.calledOnce).equal(true);
-
-
-            //    throw new Error("Not Implemented");
-
         });
 
-        it("Should return last Build from definitions triggered from the same Source Version only for the Build Definitons specified", async () => {
+        it("Should not create tag if there's a last Build Cancelling and with flag release on cancel on", async () => {
+            expect(true, "Not Implemented").false;
+        });
 
+        it("Should not create tag if there's a last Build Cancelling and with flag release on cancel off", async () => {
+            expect(true, "Not Implemented").false;
+        });
+
+        it("Should not create tag if there's a last Build Cancelling and with flag release on cancel on", async () => {
+            expect(true, "Not Implemented").false;
+        });
+
+        it("Should not create tag if there's a last Build cancelled and with flag release on cancel off", async () => {
+            expect(true, "Not Implemented").false;
+        });
+
+        it("Should not create tag if there's a last Build failed and with flag release on failure on", async () => {
+            expect(true, "Not Implemented").false;
+        });
+
+        it("Should not create tag if there's a last Build failed and with flag release on failure off", async () => {
+            expect(true, "Not Implemented").false;
+        });
+
+
+        it("Should not create tag if there's a last Build with Status Queued", async () => {
+            expect(true, "Not Implemented").false;
+        });
+
+        it("Should not create tag if there's a last Build with Status NotStarted", async () => {
+            expect(true, "Not Implemented").false;
+        });
+
+        it("Should not create tag if there's a last Build with Status InProgress", async () => {
+            expect(true, "Not Implemented").false;
+        });
+
+        it("Should not create tag if there's a last Build with Status PostPoned", async () => {
+            expect(true, "Not Implemented").false;
         });
     });
 });
